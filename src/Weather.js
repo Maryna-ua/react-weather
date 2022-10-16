9edb69c4e77b7ba32bc4a28972c1494043c52b87
Marina <kodola.marina@gmail.com>
2022-10-16 16:18:01 +0300

    alert the weather

---
+++ src/Weather.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import axios from "axios";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(
+      `The weather in ${response.data.name} is ${Math.round(
+        response.data.main.temp
+      )}°C`
+    );
+  }
+
+  let apiKey = "1c6f613839ce6b71f2df6e20804a91d8";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
+}
